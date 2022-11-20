0c5b3caafe3bae282825f3e7c46c64efd6a018f1
adrixop95 <adrixop95@me.com>
2022-11-20 15:29:06 +0100

    test ci

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ubi-checker
-Simple applicaiton that allow you check when RedHat release UBI container
+# UBI checker
+A very simple application whose purpose is to check when a container located on the RedHat Ecosystem Catalog (catalog.redhat.com) was last updated.
+
+The output of the application allows it to be consumed by [Telegraf](https://github.com/influxdata/telegraf) within the [exec Input Plugin](https://github.com/influxdata/telegraf/blob/master/plugins/inputs/exec/README.md) and then be integrated into the Grafana/Prometheus stack.
+
--- main.go
+++ main.go
@@ -11,20 +11,21 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-type Message struct {
-	Name string
-	Date string
+type ResponseMessage struct {
+	Name   string
+	Date   string
+	Status int
 }
 
 func main() {
 	repository_id := flag.String("id", "615bcf606feffc5384e8452e", "Container ID")
 	flag.Parse()
 
-	fmt.Println(get_date(*repository_id))
+	fmt.Println(telegraf_message(*repository_id))
 
 }
 
-func get_date(id string) string {
+func telegraf_message(id string) string {
 	resp, err := http.Get("https://catalog.redhat.com/api/containers/v1/repositories/id/" + id)
 	if err != nil {
 		log.Fatalln(err)
@@ -37,11 +38,11 @@ func get_date(id string) string {
 
 	value := gjson.GetMany(string(body), "repository", "last_update_date")
 
-	m := Message{value[0].String(), value[1].String()}
-	b, err := json.Marshal(m)
+	message := ResponseMessage{value[0].String(), value[1].String(), resp.StatusCode}
+	bytemessage, err := json.Marshal(message)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	return string(b)
+	return string(bytemessage)
 }
